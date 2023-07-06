31ae731a2e899ea432e54f8755145d1769587519
elliott <enh4bn@virginia.edu>
2023-07-06 00:23:46 -0400

    work

--- webScrape.py
+++ webScrape.py
@@ -1,23 +1,12 @@
 import requests
 import requests_html
 import time
+import pandas
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
-
-# fips = '01073'
-# testUrl = "https://www.countyhealthrankings.org/explore-health-rankings/county-health-rankings-model/health-outcomes/length-of-life/premature-death?keywords=&f%5B0%5D=type%3Astates&f%5B1%5D=type%3Acounties&year=2023&county="+fips
-# print("TRY:",testUrl)
-# session = requests_html.HTMLSession()
-
-# r = session.get(testUrl)
-# time.sleep(2)
-# soup = BeautifulSoup(r.html.html, 'html.parser')
-# print(soup.find("div", {"class": "_chrr2022_data-tab__content"}))
-
-def scrapeCensus():
-    countyName = "Jefferson"
-    countyDesc = "County"
-    stateTitle = "AL"
+def generateCensusLinks():
+    links = []
     states = {
             'AL': 'Alabama',
             'AK': 'Alaska',
@@ -78,10 +67,48 @@ def scrapeCensus():
             'AE': 'Armed Forces Africa/Canada/Europe/Middle East',
             'AP': 'Armed Forces Pacific'
             }
-    stateTitle = states[stateTitle]
-    urlTemplate = "https://data.census.gov/all?q="+countyName+"+"+countyDesc+",+"+stateTitle
-    print(urlTemplate)
+    xl = pandas.read_excel("2020-2023 RAM Clinic Locations (County-Level Data) (1).xlsx", sheet_name="Counties of RAM Clinics", usecols="H")
+    counties = xl.loc[1:]
+    for i in range(0,len(counties) - 1):
+        county = counties.iloc[i].values[0]
+        arr = county.split(" ")
+        arr[-1] = states[arr[-1]]
+        county = "+".join(arr)
+        urlTemplate = "https://data.census.gov/all?q="+county
+        links.append(urlTemplate)
+    return links
 
+def scrapeCensusPopulations():
+    print(" - - Scraping Census Data - - ")
+    links = generateCensusLinks()
+    for i in links:
+        print("\n Source:",i)
+        sleeptime = 0.5
+        session = requests_html.HTMLSession()
+        r = session.get(i)
+        result = []
+        while result == []:
+            sleeptime *= 2
+            r.html.render(timeout=25, sleep=sleeptime)
+            result = r.html.find(".featured-result-value")
+        print(" * * POPULATION: "+result[0].text+" * * ")
+        session.close()
+
+def scrapePrematureDeaths():
+        fips = '01073'
+        testUrl = "https://www.countyhealthrankings.org/explore-health-rankings/county-health-rankings-model/health-outcomes/length-of-life/premature-death?keywords=&f%5B0%5D=type%3Astates&f%5B1%5D=type%3Acounties&year=2023&county="+fips
+        print("TRY:",testUrl)
+        
+        session = requests_html.HTMLSession()
+        r = session.get(testUrl)
+        result = []
+        sleeptime = 6
+        r.html.render(timeout=25, sleep=sleeptime)
+        result = r.html.find("._chrr2022_measures__measure-disaggregated-table-highlighted-row")[0].html
+        soup = BeautifulSoup(result, 'html.parser')
+        print(soup.findAll("td")[1].text)
+            
 if __name__ == "__main__":
-    scrapeCensus()
+    #scrapeCensusPopulations()
+    scrapePrematureDeaths()
     
