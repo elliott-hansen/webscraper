8f47485e7c14377d2032cc6acac21cde1a7c4615
elliott <enh4bn@virginia.edu>
2023-07-08 11:57:29 -0400

    trying color

--- excelInsert.py
+++ excelInsert.py
@@ -1,12 +1,15 @@
 import pandas
+from openpyxl.styles import Font, Color, colors, fills
 
 def findPresent():
     url = "2020-2023 RAM Clinic Locations (County-Level Data) (1).xlsx"
     xl = pandas.read_excel(url, sheet_name="2023 Clinics")
     j = 2
     for i in range(0, 66):
-        xl.iloc[3, [j]] = 'TEST'
-        print(xl.iloc[3, [j]])
+        loc = xl.iloc[3, [j]]
+        loc = 'TEST'
+        loc.fill = fills.PatternFill(patternType='solid', fgColor=Color(rgb='00FF00'))
+        print(loc)
         j+=7
     xl.to_excel(url)
 if __name__ == "__main__":
